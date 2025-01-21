3c1b46a32a4c6811a61a048d57b8f17801f17cd6
matcha-mnk <noiha1161@gmail.com>
2025-01-22 01:24:32 +0900

    StoragePage追加

--- frontend/src/pages/TopPage.jsx
+++ frontend/src/pages/TopPage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const TopPage = () => {
+  const navigate = useNavigate();
+
   return(
     <div className="top-page">
       <div>
@@ -12,16 +14,16 @@ const TopPage = () => {
         <h1>キャラクターの書庫のようなWebアプリケーションです。</h1>
         <h3>創作や暇つぶし等にお使いください。</h3>
 
-        <button className="vertical-button" id="start-btn" onClick={() => useNavigate("/storage")}>
+        <button className="vertical-button" id="start-btn" onClick={() => navigate("/storage")}>
           始める
         </button>
-        <button className="vertical-button-en" id="start-btn-en" onClick={() => useNavigate("/storage")}>
+        <button className="vertical-button-en" id="start-btn-en" onClick={() => navigate("/storage")}>
           start
         </button>
-        <button className="vertical-button" id="manual-btn" onClick={() => useNavigate("")}>
+        <button className="vertical-button" id="manual-btn" onClick={() => navigate("/")}>
           使い方
         </button>
-        <button className="vertical-button-en" id="manual-btn-en" onClick={() => useNavigate("/storage")}>
+        <button className="vertical-button-en" id="manual-btn-en" onClick={() => navigate("/")}>
           manual
         </button>
       </div>
