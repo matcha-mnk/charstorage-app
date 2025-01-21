0cd75baa6eab196a013d47d97e1b316a12fec591
matcha-mnk <noiha1161@gmail.com>
2025-01-21 17:13:31 +0900

    frontend環境構築

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+
+const App = () => {
+    return (
+        <div className='App'>
+            <h1>Hello, React without CRA or Vite!</h1>
+        </div>
+    );
+};
+
+export default App;
