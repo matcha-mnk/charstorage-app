3f2a760bdd742a06d12dfdbe257a40e568055ed0
matcha-mnk <noiha1161@gmail.com>
2025-01-21 17:52:43 +0900

    .env追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';;
+import axios from 'axios';
 import './App.css';
 
 
-const App = () => {
-    return (
-        <div className='App'>
-            <h1>Hello, React without CRA or Vite!</h1>
-        </div>
-    );
-};
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_API_URL + '/')
+        .then((response) => {
+            setMessage(response.data);
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error);
+        });
+  }, []);
+
+  return (
+    <div className='App'>
+        <h1>Hello, React without CRA or Vite!</h1>
+        <p>{message}</p>
+    </div>
+  );
+}
 
 export default App;
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -6,6 +7,12 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      fallback: {
+        "process": require.resolve("process/browser")
+      }
     },
     module: {
         rules: [
@@ -22,19 +29,20 @@ module.exports = {
             },
         ],
     },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html',
         }),
+        new webpack.DefinePlugin({
+          'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL || 'http://localhost:5000')
+        })
     ],
     devServer: {
-        static: './dist',
+        static: path.join(__dirname, 'dist'),
         port: 3000,
         open: true,
         hot: true,
+        historyApiFallback: true,
     },
     mode: 'development',
 };
