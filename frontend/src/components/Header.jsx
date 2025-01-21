832c69476d993ab720a1140db1560fc81ebec327
matcha-mnk <noiha1161@gmail.com>
2025-01-22 01:45:47 +0900

    見た目調整

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { MoonSvg } from "../svgComponents/MoonSvg";
 import { SunSvg } from "../svgComponents/SunSvg";
+import { useNavigate } from "react-router-dom";
 
 const Header = ({handleToggle}) => {
+  const navigate = useNavigate();
   return (
     <header>
-      <h1 className="logo">Charstorage</h1>
+      <button className="top-button" onClick={() => navigate("")}><h1 className="logo">Charstorage</h1></button>
       <div className="theme-change-ui">
       <SunSvg size={24} color="#f4f4f4"/>
         <div className="theme-change">
